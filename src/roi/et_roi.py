72ff33e285073ac914eded956669dc432eecd909
knowledge-platformpk <158771938+knowledge-platformpk@users.noreply.github.com>
2024-03-25 06:32:18 +0500

    Update et_roi.py

--- src/roi/et_roi.py
+++ src/roi/et_roi.py
@@ -68,7 +68,7 @@ def create_et_timeseries_roi(json_data, from_date, to_date):
     fig, ax = plt.subplots(figsize=(10, 6))  # Adjust the figure size as needed
     df.plot(x='date', y='ET', ax=ax, legend=True, title='ET Time Series')
     plt.xlabel('Date', fontsize=6)
-    plt.ylabel('Mean Evapotranspiration (ET)')
+    plt.ylabel('Mean Evapotranspiration (ET) kg/m^2/8day')
     plt.grid(True)
     plt.tight_layout()
 
